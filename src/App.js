fa6eeda62cfa1ebea16f05bbf89335747ea0df8a
VENKATA NARAYANAN <prakash1107@gmail.com>
2024-04-01 07:38:25 +0800

    containers-2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import { Modal } from "./components/modals/Modal";
-import { LargeBookListItem } from "./components/books/LargeListItems";
-import { books } from "./data/books";
+import { UserLoader } from "./components/containers/user-loader";
+import { ResourceLoader } from "./components/containers/resource-loader";
+import { BookInfo } from "./components/container-children/book-info";
+import { UserInfo } from "./components/container-children/user-info";
 
 export default function App() {
   return (
     <>
-      <Modal>
-        <LargeBookListItem book={books[0]} />
-      </Modal>
+      {/* <UserLoader userId={"3"}>
+        <UserInfo />
+      </UserLoader>
+      <UserLoader userId={"2"}>
+        <UserInfo />
+      </UserLoader>
+      <UserLoader userId={"1"}>
+        <UserInfo />
+      </UserLoader> */}
+      <ResourceLoader resourceUrl={"/users/2"} resourceName={"user"}>
+        <UserInfo />
+      </ResourceLoader>
+      <ResourceLoader resourceUrl={"/books/2"} resourceName={"book"}>
+        <BookInfo />
+      </ResourceLoader>
     </>
   );
 }
